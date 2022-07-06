07a77e648de1d91f7c10978fd768224cb33d23db
etik <dogusetik@gmail.com>
2022-07-06 18:17:15 +0300

    öğrenci class'ı ikinci işlemler yapıldı

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,6 +1,8 @@
 public class Ogrenci {
     public static void main(String[] args) {
 
-        System.out.println("merhaba dünya");
+
+        System.out.println("merhaba dünya 2");
+        System.out.println("merhaba dünya 3");
     }
 }
